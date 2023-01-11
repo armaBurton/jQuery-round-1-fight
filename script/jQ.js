27e8c470ab6b5d0559378fee8b4b0d9ef9905357
armaBurton <armanarma@armaburton.com>
2023-01-11 13:12:57 -0800

    finished selector, moving to events

--- script/jQ.js
+++ script/jQ.js
@@ -3,7 +3,11 @@ const func1 = () => {
   //document.getElementByTagName(element name)
   // $("div").css("background-color", "orange");
   // $(".p1").css("font-style", "italic");
-  $("div").css("margin-bottom", "10px");
-  $("#div1, #div2").css("background-color", "green");
-  $("li:even").css("background-color", "yellow");
+  // $("div").css("margin-bottom", "10px");
+  // $("#div1, #div2").css("background-color", "green");
+  // $("li:even").css("background-color", "yellow");
+  // $("div p:last").fadeToggle();
+  // $("p:first").css("background-color", "pink");
+  // $("li:nth-child(2), li:nth-child(5)").fadeToggle();
+  $("li").filter(":nth-child(2), :nth-child(5)").fadeToggle();
 };
