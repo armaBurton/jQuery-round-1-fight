78dcf3c943049419f32fb38fee1ecf9779055150
armaBurton <armanarma@armaburton.com>
2023-01-11 12:53:18 -0800

    more jQuery selectors

--- script/jQ.js
+++ script/jQ.js
@@ -1,5 +1,9 @@
-function funct1() {
-  $("#img1").fadeToggle(3000);
-}
-
-$(document).ready(() => {});
+const func1 = () => {
+  //$(element name).action()
+  //document.getElementByTagName(element name)
+  // $("div").css("background-color", "orange");
+  // $(".p1").css("font-style", "italic");
+  $("div").css("margin-bottom", "10px");
+  $("#div1, #div2").css("background-color", "green");
+  $("li:even").css("background-color", "yellow");
+};
