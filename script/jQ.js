9e39f18b251e8da21a43a0ade26fc4dbbe0f8f87
armaBurton <armanarma@armaburton.com>
2023-01-12 08:15:20 -0800

    DOM minipulation

--- script/jQ.js
+++ script/jQ.js
@@ -14,8 +14,89 @@
 // $("li").filter(":nth-child(2), :nth-child(5)").fadeToggle();
 //************
 //EVENTS
+// $(window).on("load", () => {
+//   $("#btn").on("dblclick", () => {
+//     $("#img1").css("width", "500px");
+//   });
+// });
+//*****************
+//EFFECTS
+// $(window).on("load", () => {
+//   $("#btn1").on("click", () => {
+//     $("#img1").fadeOut(2000);
+//   });
+
+//   $("#btn2").on("click", () => {
+//     $("#img1").fadeIn(2000);
+//   });
+
+//   $("#btn3").on("click", () => {
+//     $("#img1").fadeToggle(2000);
+//   });
+// });
+// $(window).on("load", () => {
+//   $("#btn").on("click", () => {
+//     $("#img").animate(
+//       {
+//         left: "150px",
+//         opacity: "1",
+//         height: "400px",
+//         width: "400px",
+//       },
+//       2000,
+//       () =>
+//         $("#img").animate(
+//           {
+//             left: 0,
+//             opacity: "0.4",
+//             height: "250px",
+//             width: "250px",
+//           },
+//           2000
+//         )
+//     );
+//   });
+// });
+
+// const changeBack = () => {
+//   $("#img").animate(
+//     {
+//       left: 0,
+//       opacity: "0.4",
+//       height: "250px",
+//       width: "250px",
+//     },
+//     2000
+//   );
+// };
+
 $(window).on("load", () => {
-  $("#btn").on("dblclick", () => {
-    $("#img1").css("width", "500px");
+  // $(".div1").on("click", () => {
+  //   const x = $(".p1").html();
+  //   $("#span1").text(x);
+  // });
+  // $(".div1").on("click", () => {
+  //   console.log($(".p1").html());
+  //   console.log($(".p1").text());
+  // });
+  // $("#btn1").on("click", () => {
+  //   $(".p1").attr("id", "idp2");
+  // });
+  // $(".p1").on("click", () => {
+  //   console.log(`|| $('.p1').attr('id')) >`, $(".p1").attr("id"));
+  // });
+  const getRGBColor = () => {
+    const r = Math.floor(Math.random() * 255);
+    const g = Math.floor(Math.random() * 255);
+    const b = Math.floor(Math.random() * 255);
+
+    return `rgb(${r}, ${g}, ${b})`;
+  };
+
+  $("#btn1").on("click", () => {
+    $(".div1").css("background-color", getRGBColor());
+  });
+  $(".div1").on("click", () => {
+    console.log($(".div1").css("background-color"));
   });
 });
